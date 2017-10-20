680a99690062e831b896e71380499dd1bbd0dd5d
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-10-20 16:46:55 -0400

    update script

--- example_udf_script.py
+++ example_udf_script.py
@@ -1,1 +1,1 @@
-import numpy as np; np.random.seed(); mean_val = df[col]; df['sim_col'] = np.random.poisson(mean_val, len(df))
+import numpy as np; np.random.seed(); mean_val = df[col].mean(); df['sim_col'] = np.random.poisson(mean_val, len(df))
